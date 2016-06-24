1444d0d87f1758b8af6b9e664fb18c206747094b
Lynx <Lynx@Lynxs-Air.fios-router.home>
2016-06-23 23:04:07 -0400

    add existing files

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Lynx on 6/23/16.
+//  Copyright © 2016 Lynx. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
---
+++ LionsAndTigers/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  LionsAndTigers
+//
+//  Created by Lynx on 6/23/16.
+//  Copyright © 2016 Lynx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        print("My tigers name is: \(myTiger.name), it's age is \(myTiger.age), it's breed is \(myTiger.breed) and it's image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func nextBarButton(sender: UIBarButtonItem) {
+    }
+
+}
+
